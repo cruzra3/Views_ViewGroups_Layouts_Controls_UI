dcac6c8bdbdc4973b1982a5f471a46de7f375598
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-04 11:34:55 -0400

    Added test with frame layout using layout gravity.

--- app/src/main/java/com/raco/views_viewgroups_layouts_controls_ui/MainActivity.java
+++ app/src/main/java/com/raco/views_viewgroups_layouts_controls_ui/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.linear_layout_example_multiple_weights);
 //        setContentView(R.layout.linear_layout_example_gravity);
 //        setContentView(R.layout.linear_layout_example_layout_gravity);
-        setContentView(R.layout.frame_layout_example);
+        setContentView(R.layout.frame_layout_example_layout_gravity);
     }
 }
