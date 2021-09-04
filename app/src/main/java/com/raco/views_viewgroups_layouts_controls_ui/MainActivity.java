2e34f248df383224000ad46c20190d509cc8a52d
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-04 09:57:02 -0400

    Initial commit, minimum changes so far.

---
+++ app/src/main/java/com/raco/views_viewgroups_layouts_controls_ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.raco.views_viewgroups_layouts_controls_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
