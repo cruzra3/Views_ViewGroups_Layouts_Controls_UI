7e0c61af5d31d8dc56aa9a9d46fd61af5aade47b
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-05 12:28:54 -0400

    Add exercise layout from chapter 5 of Head First Android Development book.  The exercise consist in creating a layout with some views on it.

--- app/src/main/java/com/raco/views_viewgroups_layouts_controls_ui/MainActivity.java
+++ app/src/main/java/com/raco/views_viewgroups_layouts_controls_ui/MainActivity.java
@@ -9,11 +9,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout_exercise);
 //        setContentView(R.layout.activity_main);
 //        setContentView(R.layout.linear_layout_example);
 //        setContentView(R.layout.linear_layout_example_multiple_weights);
 //        setContentView(R.layout.linear_layout_example_gravity);
 //        setContentView(R.layout.linear_layout_example_layout_gravity);
-        setContentView(R.layout.frame_layout_example_with_nested_linear_layout);
+//        setContentView(R.layout.frame_layout_example_with_nested_linear_layout);
     }
 }
